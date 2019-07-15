3ed71b7aa8a4e5277a9dd8046035b321da8a779c
psymchoi <52904734+psymchoi@users.noreply.github.com>
2019-07-15 15:16:57 +0900

    테스트1
    테스트용.

---
+++ Assets/BaseGameManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseGameManager : MonoBehaviour
+{
+    public enum eLoadingState
+    {
+        NONE    = 0,
+        START,
+        UNLOADING,
+        LOADING,
+        END
+    }
+
+    public enum eStageState
+    {
+        NONE    = 0,
+        LOBBY,
+        INGAME,
+    }
+
+    public static BaseGameManager _uniqueinstance;
+
+    GameObject _GameToLobbyWnd;         // 게임 끝난 후 로비 진입 오브젝트.
+
+    eLoadingState _curStateLoading;
+    eStageState _curStage;
+
+    public static BaseGameManager INSTANCE
+    {
+        get { return _uniqueinstance; }
+    }
+    public eLoadingState LOADSTAGE
+    {
+        get { return _curStateLoading; }
+    }
+    public eStageState CURSTAGE
+    {
+        get { return _curStage; }
+        set { _curStage = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _uniqueinstance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
